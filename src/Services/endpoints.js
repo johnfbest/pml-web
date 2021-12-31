e0f906bd69eef6cd1af95e079e9c1a5185692ed8
John Best <john@digitaldownbeat.com>
2021-12-31 16:47:16 -0600

    changes for heroku"

--- src/Services/endpoints.js
+++ src/Services/endpoints.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const WEB_SERVER = 'http://localhost:8000';
+const WEB_SERVER = window.location.hostname;
 
 const routes = {
     libraryList: WEB_SERVER + '/collections/library/list',
